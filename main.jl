4c076cfd849095520e3c8ecdadd07fcfa05daeb9
Andrzej Szary <aszary@gmail.com>
2026-03-09 09:37:20 +0100

    obsolete removed

--- main.jl
+++ main.jl
@@ -172,46 +172,6 @@ module NoName
     end
 
 
-   function generate_signal_obsolete()
-        psr = Pulsar()
-
-        Lines.calculate_polarcaps!(psr)
-
-        #Field.calculate_dipole!(psr)
-
-        Lines.init_line_of_sight_obsolete(psr, num=200) # TODO work on that
-        Lines.calculate_line_of_sight(psr)
-
-        # TODO work on skip_steps to ensure only single pulses
-        #Sparks.init_sparks1!(psr ;num=5)
-        #Sparks.simulate_sparks(psr; n_steps=2000, skip_steps=20, speedup=10)
-        #Sparks.save_sparks(psr; num=1)
-
-        Sparks.load_sparks(psr; num=2)
-        Signal.generate_signal(psr; noise_level=0.05)
-        Signal.generate_pulses(psr)
-        Signal.generate_longitudes_obsolete(psr) # TODO work on that
-        #Signal.generate_longitudes_obsolete1(psr)
-        
-        # CHECKING
-        x,y,z = psr.los_lines[end][1][1], psr.los_lines[end][2][1], psr.los_lines[end][3][1]
-        sph = Functions.cartesian2spherical([x,y,z])
-        rho = rad2deg(Signal.rho_from_theta(sph[2]))
-        println("rho: $rho")
-        println("theta=", rad2deg(sph[2]))
-        W = Signal.pulse_width_deg(psr.alpha, psr.beta, rho)
-        println("alpha = $(psr.alpha) beta = $(psr.beta)")
-        println("Szerokość profilu teoretyczna: $(round(W, digits=2))°")
-        println("Szerokość profilu z symulacji ", psr.longitudes[end]-psr.longitudes[1], " deg.")
-
-        Plot.signal(psr)
-        Plot.pulses(psr)
-        #Plot.pulses0(psr)
-        #Plot.pulses1(psr)
-        
-    end
-
-
     function generate_signal()
         psr = Pulsar("input/1.json")
 
@@ -232,9 +192,7 @@ module NoName
         Signal.generate_signal(psr; noise_level=0.05)
         Signal.generate_pulses(psr)
         
-        # CHECKING in obsolete...
-
-        Plot.signal(psr)
+        #Plot.signal(psr)
         Plot.pulses(psr)
         #Plot.pulses0(psr)
         #Plot.pulses1(psr)
@@ -247,10 +205,9 @@ module NoName
         #small_grids()
         #full_plus_smallgrids()
 
-        #generate_signal_obsolete()
-        #generate_signal()
+        generate_signal()
 
-        LBC.animate(;ntime=200, th_cap=30.0, a_cap=15.0, b_cap=5.0, co_angl=45.0, h_sprk=3, h_drft=0.1)
+        #LBC.animate(;ntime=200, th_cap=30.0, a_cap=15.0, b_cap=5.0, co_angl=45.0, h_sprk=3, h_drft=0.1)
 
         println("Bye")
     end
